2c54455885e7e3ae17c31abf25077cba3694e51e
muchadhesion <github@morganism.info>
2022-03-27 23:55:25 +0100

    added waft

--- frac.py
+++ frac.py
@@ -7,20 +7,38 @@ import math
 WIDTH=640
 HEIGHT=400
 
+SEG_LEN=100
+SEG_GROWTH=0.85
+SEG_LEAN=20
+SPROUT_DEG=-70
+SPROUT_GROWTH=0.5
+
+MAX_DEPTH=10
+MAX_SEGS=10
+
+frame = 0
+
 def grow(screen, start, angle_deg, len, depth=1, segment=1, sprout=False):
-    a = math.radians(angle_deg)
+    a = math.radians(angle_deg )
     delta = (len * math.cos(a), len * math.sin(a) )
     end = tuple( map( operator.add, start, delta ) )
     pg.draw.line(screen, pg.Color('blue'), start, end, 2)
-    if depth <= 4:
+    waft = 2 * math.sin( frame / 500)
+    if len < 4:
+        return
+    if depth <= MAX_DEPTH:
         if sprout:
-            grow(screen, end, angle_deg-80, len/2, depth=depth+1, segment=segment, sprout=True)
-    if segment <= 4:
-        grow(screen, end, angle_deg+10, len-10, depth, segment=segment+1, sprout=True)
+            grow(screen, end, angle_deg+SPROUT_DEG+waft, len*SPROUT_GROWTH, depth=depth+1, segment=segment, sprout=True)
+    if segment <= MAX_SEGS:
+        grow(screen, end, angle_deg+SEG_LEAN+waft, len*SEG_GROWTH, depth, segment=segment+1, sprout=True)
 
 def update(screen):
+    global frame
     start = (WIDTH/2, HEIGHT)
-    grow(screen, start, -90, 100, depth=2, sprout=True)
+    a = -90
+
+    grow(screen, start, a, 100, depth=2, sprout=True)
+    frame += 1
 
 def main():
     pygame.init()
@@ -28,6 +46,7 @@ def main():
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     running = True
     while running:
+        screen.fill((0,0,10))
         update(screen)
         pygame.display.flip()
 
