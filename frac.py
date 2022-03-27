1862d3a8979d28211e528d1564383eab8a6c5ce0
muchadhesion <github@morganism.info>
2022-03-27 23:55:26 +0100

    add comments

--- frac.py
+++ frac.py
@@ -5,9 +5,11 @@ import pygame as pg
 import math
 import random
 
-WIDTH=640
-HEIGHT=800
+# pygame window params
+WINDOW_WIDTH=640
+WINDOW_HEIGHT=800
 
+## fern parameters
 SEG_LEN=100
 SEG_GROWTH=0.90
 SEG_LEAN=15
@@ -21,10 +23,18 @@ SPROUT2_GROWTH=0.43
 MAX_DEPTH=16
 MAX_SEGS=20
 
+# globals
 frame = 0
 game_time_ms = 0
 
+# grow a segment of fern
+# each segment will grow at the angle_deg supplied to the length supplied
+# each segment will optionally sprout a left and right offshoot
+# terminating conditions are length of sprout, depth and segment count
+#
+# add in a "waft" of wind to animate the fern
 def grow(screen, start, angle_deg, len, depth=1, segment=1, sprout=False):
+    # calculat and draw this segment trunk
     a = math.radians(angle_deg )
     delta = (len * math.cos(a), len * math.sin(a) )
     end = tuple( map( operator.add, start, delta ) )
@@ -32,28 +42,31 @@ def grow(screen, start, angle_deg, len, depth=1, segment=1, sprout=False):
     t = game_time_ms / 1000
     waft_extra = random.random() * 0
     waft = 10 * math.sin( t * 0.5 ) + math.sin( waft_extra)
+
+    # check if we want to draw sprouted offshoots
     if len < 4:
+        # short limb, don't do any more sprouting
         return
     if depth <= MAX_DEPTH:
         if sprout:
+            # sprout a left and right offshoot
             grow(screen, end, angle_deg+SPROUT_DEG+waft, len*SPROUT_GROWTH, depth=depth+1, segment=segment, sprout=True)
             grow(screen, end, angle_deg+SPROUT2_DEG+waft, len*SPROUT2_GROWTH, depth=depth+1, segment=segment, sprout=True)
     if segment <= MAX_SEGS:
+        # grow the next segment
         grow(screen, end, angle_deg+SEG_LEAN+waft, len*SEG_GROWTH, depth, segment=segment+1, sprout=True)
 
 def update(screen):
-    global frame
-    
-    start = (WIDTH/2, HEIGHT)
-    a = -90
+    start = (WINDOW_WIDTH/2, WINDOW_HEIGHT)
+    grow(screen, start, -90, 100, depth=2, sprout=True)
 
-    grow(screen, start, a, 100, depth=2, sprout=True)
-    frame += 1
-
+#
+# Create a pygame window and "game frame" boilerplate
+#
 def main():
     pygame.init()
 
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
     running = True
     clock = pygame.time.Clock()
 
@@ -62,9 +75,12 @@ def main():
         ms = clock.tick(60)
         game_time_ms += ms
         screen.fill((0,0,10))
-        update(screen)
-        pygame.display.flip()
 
+        # draw the fern
+        update(screen)
+
+        pygame.display.flip()
+        # check exit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -73,5 +89,3 @@ def main():
 if __name__=="__main__":
     # call the main function
     main()
-
-
